b37e2a6c09fdd61b6bd898b7b29df45d2a33ccc7
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-21 21:18:35 -0300

    add initial maven setup

---
+++ src/main/java/com/bookly/BooklyApplication.java
@@ -0,0 +1,13 @@
+package com.bookly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklyApplication.class, args);
+	}
+
+}
