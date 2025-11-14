c155471b7594285a3d09c364ee6e310d4c307317
Arthur Silveira <arthurmarques2148@gmail.com>
2025-11-14 16:20:11 -0300

    feat: cria UsuarioMapper.

---
+++ src/main/java/com/bookly/mapper/UsuarioMapper.java
@@ -0,0 +1,13 @@
+package com.bookly.mapper;
+
+import com.bookly.dto.UsuarioPostRequestBody;
+import com.bookly.dto.UsuarioPutRequestBody;
+import com.bookly.model.Usuario;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+    Usuario toUsuario(UsuarioPostRequestBody usuarioPostRequestBody);
+
+    Usuario toUsuario(UsuarioPutRequestBody usuarioPutRequestBody);
+}
