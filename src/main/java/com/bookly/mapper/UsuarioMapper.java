7ce7ab84b92fc2231887f1fa7baacec993607aed
Arthur Silveira <arthurmarques2148@gmail.com>
2025-11-14 17:05:24 -0300

    feat: Adiciona métodos de mapeamento de saída (Entidade para UsuarioGetResponse)

--- src/main/java/com/bookly/mapper/UsuarioMapper.java
+++ src/main/java/com/bookly/mapper/UsuarioMapper.java
@@ -1,13 +1,19 @@
 package com.bookly.mapper;
 
+import com.bookly.dto.UsuarioGetResponse;
 import com.bookly.dto.UsuarioPostRequestBody;
 import com.bookly.dto.UsuarioPutRequestBody;
 import com.bookly.model.Usuario;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
     Usuario toUsuario(UsuarioPostRequestBody usuarioPostRequestBody);
 
     Usuario toUsuario(UsuarioPutRequestBody usuarioPutRequestBody);
+
+    UsuarioGetResponse toResponse(Usuario usuario);
+    List<UsuarioGetResponse> toResponseList(List<Usuario> usuarios);
 }
