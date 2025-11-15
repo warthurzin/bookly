f1163bc1ba04734f49074a8e1a790c6f9bffa4db
Arthur Silveira <arthurmarques2148@gmail.com>
2025-11-15 15:12:38 -0300

    feat: Adiciona query method findByNome para busca de usuário.

--- src/main/java/com/bookly/repository/UsuarioRepository.java
+++ src/main/java/com/bookly/repository/UsuarioRepository.java
@@ -3,10 +3,13 @@ package com.bookly.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.bookly.model.Usuario;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmail(String email);
+    List<Usuario> findByNome(String nome);
 }
