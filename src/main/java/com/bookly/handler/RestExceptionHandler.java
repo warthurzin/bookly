aa9d623d165c45894a310fb1ae7b8cc4d6f5ca39
Arthur Silveira <arthurmarques2148@gmail.com>
2025-11-21 20:53:43 -0300

    feat: Adiciona tratamento global para MethodArgumentNotValidException

--- src/main/java/com/bookly/handler/RestExceptionHandler.java
+++ src/main/java/com/bookly/handler/RestExceptionHandler.java
@@ -2,12 +2,16 @@ package com.bookly.handler;
 
 import com.bookly.exception.BadRequestException;
 import com.bookly.exception.BadRequestExceptionDetails;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class RestExceptionHandler {
@@ -21,6 +25,24 @@ public class RestExceptionHandler {
                         .title("Requisição Inválida, Cheque a Documentação")
                         .details(bre.getMessage())
                         .developerMessage(bre.getClass().getName())
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException manvException) {
+
+        String fieldsMessage = manvException.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                BadRequestExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Campos Inválidos - Verifique a requisição")
+                        .details("Os campos a seguir estão incorretos: " + fieldsMessage) // Mensagem mais amigável
+                        .developerMessage(manvException.getClass().getName())
                         .build(), HttpStatus.BAD_REQUEST);
     }
 }
