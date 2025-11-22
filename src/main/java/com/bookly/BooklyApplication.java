10f7783362ebcc86fa0c24524f349acc335dea54
Arthur Silveira <arthurmarques2148@gmail.com>
2025-11-22 16:57:10 -0300

    perf: Adiciona @EnableSpringDataWebSupport para estabilizar a serialização JSON da paginação.

--- src/main/java/com/bookly/BooklyApplication.java
+++ src/main/java/com/bookly/BooklyApplication.java
@@ -2,8 +2,10 @@ package com.bookly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class BooklyApplication {
 
 	public static void main(String[] args) {
