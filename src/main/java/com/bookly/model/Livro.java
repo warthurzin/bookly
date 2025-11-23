2d0d4a8148ec2d967c62b1fc8d175d44d6ad3872
Luanna <luannaevellyn2020@gmail.com>
2025-11-23 13:55:17 -0300

    feat: criar entidade Livro com atributos e mapeamentos JPA

---
+++ src/main/java/com/bookly/model/Livro.java
@@ -0,0 +1,59 @@
+package com.bookly.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long usuarioId;
+
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private String editora;
+    private Integer anoPublicacao;
+
+    @Lob
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    private CondicaoLivro condicaoLivro;
+
+    @Enumerated(EnumType.STRING)
+    private TipoDisponibilidade tipoDisponibilidade;
+
+    @Enumerated(EnumType.STRING)
+    private StatusLivro statusLivro;
+
+    private String fotoCapa;
+    private Integer numeroPaginas;
+    private String idioma;
+
+    @CreationTimestamp
+    @Column(name = "data_cadastro", updatable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataCadastro;
+
+    @UpdateTimestamp
+    @Column(name = "data_atualizacao")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataAtualizacao;
+
+}
